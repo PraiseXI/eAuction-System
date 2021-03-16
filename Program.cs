6f37cd8f034ae81061bee9be9137e9d4b5ecc471
praise <praisexi@outlook.com>
2021-03-16 19:13:55 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eAuction_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

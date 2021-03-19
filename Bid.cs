3d3307b90413bbc78b2e09d371d69698cc66217b
praise <praisexi@outlook.com>
2021-03-19 23:24:31 +0000

    bid class changes

---
+++ Bid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eAuction_System
+{
+    public class Bid
+    {
+        Random rnd = new Random();
+
+        private int bidID = rnd;
+        private int auctionID;
+        private int buyerID;
+        private double amount;
+        private DateTime when;
+
+        public Bid(double Amount)
+        {
+
+        }
+    }
+    // private int BidID = rnd.Next(1, 5000);
+}

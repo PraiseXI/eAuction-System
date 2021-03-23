25d49f0bfdb36948150ffd1961c31dd49c8f83ce
praise <praisexi@outlook.com>
2021-03-23 22:47:45 +0000

    added string check method

--- Program.cs
+++ Program.cs
@@ -4,14 +4,21 @@ namespace eAuction_System
 {
     class Program
     {
+        public static void stringCheck(string word)
+        {
+            do
+            {
+                if (String.IsNullOrEmpty(word) == true)
+                {
+                    Console.WriteLine("Entry can't be empty, please try again: ");
+                    Console.ReadLine();
+                }
+            }
+            while (String.IsNullOrEmpty(word) == true);
+        }
         static void Main(string[] args)
         {
-            user Praise = new user();
-            Praise.setUsername("praise11");
-            string username = Praise.getUsername();
-            Console.WriteLine(username);
             Console.ReadLine();
-
         }
     }
 }

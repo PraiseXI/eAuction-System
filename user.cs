86376e8a6c58b2f8d2b2076135f2f3cb715814b9
praise <praisexi@outlook.com>
2021-03-23 17:20:17 +0000

    user class setup

---
+++ user.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eAuction_System
+{
+    public class user
+    {
+        Random random = new Random();
+        private int userID;
+        private string username;
+        private string password;
+        public void setuserID()
+        {
+            //TODO: Make sure that the random numbers are unique
+            userID = random.Next(1, 5000);
+        }
+        public int getBidID()
+        {
+            return userID;
+        }
+
+        public void setUsername(string usrnme)
+        {
+            username = usrnme;
+        }
+        public string getUsername()
+        {
+            return username;
+        }
+
+        public void setPassword(string psswrd)
+        {
+            password = psswrd;
+        }
+        public string getPassword()
+        {
+            return password;
+        }
+    }
+}

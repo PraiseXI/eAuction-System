5ae70d3d2fae2cd5157cc8a513490be1c69d640d
praise <praisexi@outlook.com>
2021-04-01 21:43:14 +0100

    item class changes

--- item.cs
+++ item.cs
@@ -1,27 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace eAuction_System
 {
     class item
     {
+        Random random = new Random();
+        private int itemID;
+        private string title;
         private string description;
 
-        public item (string desc)
+        public item (string title, string desc)
         {
-            this.description = desc;
+            this.itemID = random.Next(1, 1000);
+            setTitle(title);
+            setDescription(desc);
         }
         public void setDescription(string itemDesc)
         {
             this.description = itemDesc;
         }
-
         public string getDescription()
         {
             return description;
         }
-
-
+        public void setTitle(string title)
+        {
+            this.title = title;
+        }
+        public string getTitle()
+        {
+            return title;
+        }
     }
 }

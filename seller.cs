a2dcc03a1479964f37acd5c193fc72c2bd59d365
praise <praisexi@outlook.com>
2021-04-01 23:15:08 +0100

    create seller class + changes

---
+++ seller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eAuction_System
+{
+    public class seller : user
+    {
+        private string firstName { get; set; }
+        private string lastName { get; set; }
+
+        private int sellerID;
+
+        public seller (int num, string usrname, string passwrd) : base(num, usrname, passwrd)
+        {
+            sellerID = base.getUserID();
+        }
+
+    }
+}

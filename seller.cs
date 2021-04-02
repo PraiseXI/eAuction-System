5a61de0f3940d3b7041faeb1e06f84191b2c1d8e
praise <praisexi@outlook.com>
2021-04-03 00:05:57 +0100

    seller class changes

--- seller.cs
+++ seller.cs
@@ -6,15 +6,34 @@ namespace eAuction_System
 {
     public class seller : user
     {
-        private string firstName { get; set; }
-        private string lastName { get; set; }
-
-        private int sellerID;
-
-        public seller (int num, string usrname, string passwrd) : base(num, usrname, passwrd)
+        //list is used as the storage size will constantly change
+        private LinkedList<item> itemslist = new LinkedList<item>();
+        public seller (int num, string usrname, string passwrd) : base(num, convertToLower(usrname), convertToLower(passwrd)
         {
-            sellerID = base.getUserID();
+
+        }
+        private static string convertToLower(string text)
+        {
+            return text.ToLower();
+        }
+        public LinkedList<item> getAllItems()
+        {
+            return itemslist;
         }
 
+        /*
+         * wanted to implement a way to delete an item however it is not required, will go back if I have time
+        public item deleteItem(int idNum)
+        {
+            foreach (item thing in itemslist)
+            {
+                if (thing.getID().Equals(idNum))
+                {
+                    context.DeleteObject
+                }
+            }
+            return null;
+        }
+        */
     }
 }

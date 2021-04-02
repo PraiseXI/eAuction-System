d658da80db179c0ae61f6e9a6f5bd47b0b3a408e
praise <praisexi@outlook.com>
2021-04-03 00:15:05 +0100

    buyer class changes

---
+++ buyer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eAuction_System
+{
+    public class buyer : user
+    {
+        public buyer(int num, string usrname, string passwrd) : base(num, convertToLower(usrname), convertToLower(passwrd))
+        {
+
+        }
+        private static string convertToLower(string text)
+        {
+            return text.ToLower();
+        }
+    }
+}

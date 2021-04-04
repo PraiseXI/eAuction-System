efa64099551f212534b6bd02f31792a41844c28c
praise <praisexi@outlook.com>
2021-04-04 18:31:34 +0100

    created class

---
+++ States.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eAuction_System
+{
+    public enum States
+    {
+        PENDING, ACTIVE, CLOSED, BLOCKED
+    }
+}

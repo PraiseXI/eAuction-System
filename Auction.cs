01b20973034b81e2ca94ebb6dfe59efdc3b1d997
praise <praisexi@outlook.com>
2021-04-08 22:57:30 +0100

    changes

--- Auction.cs
+++ Auction.cs
@@ -20,36 +20,43 @@ namespace eAuction_System
         private Item auctionItem;
         private Seller auctionSeller;
         private States state;
+        private Buyer winner;
+        private Bid highestBid;
 
-        public Auction(double startPrice, double reservePrice, DateTime closingDate, Item auctionitem, Seller auctionSeller, States state)
+        public Auction(double startPrice, double reservePrice, DateTime closingDate, Item auctionitem, Seller auctionSeller)
         {
             this.auctionID = random.Next(1, 5000);
             this.startingPrice = startPrice;
             this.reservePrice = reservePrice;
-            this.closingDate = closingDate;
+            do
+            {
+                if (checkCloseDate(closingDate))
+                {
+                    this.closingDate = closingDate;
+                }
+                else
+                {
+                    Console.WriteLine("You are allowed to have a closing date 7 or less days that the current date");
+                }
+                //TODO: format date before its been set
+            }
+            while (checkCloseDate(closingDate) == false);
             this.auctionItem = auctionitem;
             this.auctionSeller = auctionSeller;
-            this.state = state;
+            this.state = States.PENDING;
         }
         public void setAuctionID()
         {
             //TODO: Make sure that the random numbers are unique
             this.auctionID = random.Next(1, 5000);
-        }      public int getAuctionID()
+        }
+        public int getAuctionID()
         {
             return auctionID;
-        }
-        public void setStartPrice(double price)
-        {
-            this.startingPrice = price;
         }
         public double getStartPrice()
         {
             return startingPrice;
-        }
-        public void setReservePrice(double price)
-        {
-            this.reservePrice = price;
         }
         public double getReservePrice()
         {
@@ -81,8 +88,17 @@ namespace eAuction_System
         {
             return this.auctionSeller;
         }
+        public List<Bid> getAllBids()
+        {
+            return this.bidList;
+        }
+        public void setState(States status)
+        {
+            this.state = status;
+        }
         public void placeBid(int auctionID, int buyerID, double howMuch, DateTime when)
         {
+            //TODO: checks for amount and datetime
             new Bid(auctionID, buyerID, howMuch, when);
         }
         public States verifying()
@@ -92,15 +108,48 @@ namespace eAuction_System
         //list should already be arranged as you cannot add a bid unless it is greater than previous, so i just need to select the last element
         public int getHighestBid()
         {
+            Bid winner = Enumerable.Max(getAllBids(), Comparer.C
             //TODO: add error check for if there is no bids.
-            var highest = bidList.Last().getBidID();
+            //last element should be the highest as it adds higher bids.
+            int highest = bidList.Last().getBuyerID();
             return highest;
         }
-        /*
+        public bool checkCloseDate(DateTime dateToCheck)
+        {
+            if ((dateToCheck - DateTime.Now).TotalDays <= 7)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+                //TODO: format date before its been set
+        }
         public string displayAuction()
         {
+            return String.Format("This auction is #{0} by {1} for {2}, with a starting price of £{3}, with a reserve price of: £{4}, which closes on: {5}", this.auctionItem.getID(), this.auctionSeller, this.auctionItem.getDescription(), this.startingPrice, this.reservePrice, this.closingDate);
+        }
+        /*
+        public bool checkBidAmount(double amount)
+        {
+            //20% upper, 10% lower
+            double lower = startingPrice + (startingPrice / 10);
+            double upper = startingPrice + (startingPrice / 20);
+            // IEnumerable<double> range = Enumerable.Range(lower, upper).Contains();
+            foreach (Bid b in bidList)
+            {
+            if (IEnumerable<double> range = Enumerable.Range(lower, upper).Contains( ))
+                {
+
+                }
+            }
+
 
         }
         */
+
+
+
     }
 }

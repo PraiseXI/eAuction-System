67143ccb9a1bf04d253663f30aead8f71a912a10
praise <praisexi@outlook.com>
2021-04-09 01:03:03 +0100

    created class

---
+++ AuctionChecker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eAuction_System
+{
+    class AuctionChecker
+    {
+        //will periodically check auctions and close them if they are outdated
+    }
+}

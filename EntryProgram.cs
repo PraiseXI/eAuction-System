0e0f402eb066c4eff8047488f18cd6c5e83e1c04
praise <praisexi@outlook.com>
2021-04-09 01:09:04 +0100

    class created

---
+++ EntryProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eAuction_System
+{
+    class EntryProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}

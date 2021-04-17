27f01ba735b5f0825c981dc41ce2d87cf4922035
praise <praisexi@outlook.com>
2021-04-17 21:41:10 +0100

    slight changes

--- Main/EntryProgram.cs
+++ Main/EntryProgram.cs
@@ -9,7 +9,7 @@ namespace eAuction_System
             while(true)
             {
                 AuctionSystem a = new AuctionSystem();
-                a.startMenu();
+                a.buyerMenu();
             }
 
         }

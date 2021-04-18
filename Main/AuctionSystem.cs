e9855d90d0c25ec091a973b97c7d2c407b7111dd
praise <praisexi@outlook.com>
2021-04-18 02:29:44 +0100

    major changes for functionality

--- Main/AuctionSystem.cs
+++ Main/AuctionSystem.cs
@@ -10,13 +10,26 @@ namespace eAuction_System
 {
     public class AuctionSystem
     {
+        //TODO: press a button at any time to go back to main menu
         //TODO: add option to delete account if logged in
+        //TODO: implement a way for seller to view bid history
         private List<User> allUsers = new List<User>();
+        List<Auction> activeAuctions = new List<Auction>();
         string usrnme = null;
         string psswrd = null;
         User activeUser = null;
         string activeMenu = "main";
-        public void start()
+        Seller currentSeller;
+        Buyer currentBuyer;
+        DateTime currDate = DateTime.Now;
+
+        public void systemSetup()
+        {
+            populateAuctions();
+            //will put all serialisation and adding users
+
+        }
+        public void startMenu()
         {
             int response;
             Menus menu = new Menus();
@@ -32,7 +45,7 @@ namespace eAuction_System
                 {
                     case 0:
                         Console.WriteLine("Now Exiting....Goodbye");
-                        Thread.Sleep(3000);
+                        Thread.Sleep(2000); //delays execution, just for effect :)
                         Environment.Exit(0);
                         break;
                     case 1:
@@ -42,22 +55,56 @@ namespace eAuction_System
                         createAccount();
                         break;
                     case 3:
-                        Console.WriteLine("3");
+                        browseAuctions();
                         break;
                     default:
                         Console.WriteLine("Your option must be a number (0-3) try again.");
                         break;
                 }
             } while (response > 3 || response != 0);
-
-            Console.WriteLine("It's going to start again");
-            Console.WriteLine();
-
         }
+        public void buyerMenu()
+        {
+            int response;
+            Menus menu = new Menus();
+            do
+            {
+                response = menu.buyerMenu();
 
+                int caseSwitch = response;
+
+                switch (caseSwitch)
+                {
+                    case 0:
+                        Console.WriteLine("Now Exiting....Goodbye");
+                        Thread.Sleep(2000);
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+                        browseAuctions();
+                        break;
+                    case 2:
+                        //bidonitem
+                        bidOnItem();
+                        break;
+                    case 3:
+                        //view all won auctions
+                        browseAuctions();
+                        break;
+                    case 4:
+                        //log out
+                        Console.WriteLine("Logging out...");
+                        Thread.Sleep(2000);
+                        startMenu();
+                        break;
+                    default:
+                        Console.WriteLine("Your option must be a number (0-4) try again.");
+                        break;
+                }
+            } while (response > 4 || response != 0);
+        }
         //checkbid amount method
         //check state of auction -  if(getState().Equals(States.ACTIVE))
-
         private void login()
         {
             Console.WriteLine("\n-- Log in --\n");
@@ -85,14 +132,16 @@ namespace eAuction_System
                     if (findUsername(usrnme).passCheck(psswrd)) //checks password alongside username
                     {
                         activeUser = user;
-                        //checks to see if seller or buyer to change menu
+                        //checks to see if seller or buyer to change menu and to cast it to the current, so you know who is logged in
                         if (activeUser is Seller)
                         {
                             activeMenu = "Seller";
+                            currentSeller = (Seller)activeUser;
                         }
                         else if (activeUser is Buyer)
                         {
                             activeMenu = "Buyer";
+                            currentBuyer = (Buyer)activeUser;
                         }
                     }
                 }
@@ -159,30 +208,121 @@ namespace eAuction_System
                 allUsers.Add(new Seller(newUsrnme, newPass));
             }
             Console.WriteLine("\n Your account has been successfully created");
-
+            Thread.Sleep(2000);
+            login();
         }
         //saves time instead of doing a foreach through every user
         private void browseAuctions()
         {
             Console.WriteLine("\n -- All ACTIVE AUCTIONS -- \n");
-            List<Auction> activeAuctions = new List<Auction>();
-            foreach (Auction auc in activeAuctions)
-            {
-                if(auc.getState().Equals(States.ACTIVE))
-                {
-                    activeAuctions.Add(auc);
-                }
-            }
             if (activeAuctions.Count == 0)
             {
-                Console.WriteLine("--There is no active auctions currently, please try again at another time");
+                Console.WriteLine("-- There is no active auctions currently, please try again at another time --");
             }
             foreach (Auction auction in activeAuctions)
             {
                 Console.WriteLine(auction.displayAuction());
             }
+            Thread.Sleep(3000);
             //TODO: redirect to whether they want to bid on auction
-    }
+        }
+        private void bidOnItem()
+        {
+            bool exists = false;
+            int aucID = 0;
+            double amount = 0;
+            string response = "";
+            //included the option to preview auctions first
+            do
+            {
+                Console.WriteLine("Would you like to browse all active auctions first? (y/n)");
+                response = Console.ReadLine().ToLower();
+            } while (String.IsNullOrEmpty(response) && !isAlphabet(response));
+            if (response == "y")
+            {
+                browseAuctions();
+            }
+            else if (response == "n")
+            {
+                int value = 0;
+                Console.WriteLine("-- Please enter the details of the auction you would like to bid on -- ");
+
+                do
+                {
+                    Console.Write("ID number of auction: ");
+                    while (!int.TryParse(Console.ReadLine(), out value))
+                    {
+                        Console.WriteLine("That is not a valid response please try again.");
+                        Console.Write("Please enter the ID number of the auction: ");
+                    }
+                    aucID = value;
+
+
+                    foreach (Auction auc in activeAuctions)
+                    {
+                        if (auc.getAuctionID() == aucID)
+                        {
+                            exists = true;
+                            break;
+                        }
+                    }
+                    if (exists == false)
+                    {
+                        Console.WriteLine("An auction with that ID number doesn't exist please try again");
+                    }
+                } while (exists == false);
+
+
+                Console.Write("Amount you want to bid for: £");
+                while (!int.TryParse(Console.ReadLine(), out value))
+                {
+                    Console.WriteLine("That is not a valid response please try again.");
+                    Console.Write("Please enter the amount you would like to bid: £");
+                }
+                amount = value;
+
+                foreach (Auction auc in activeAuctions)
+                {
+                    if (amount >= auc.getStartPrice())
+                    {
+                        auc.placeBid(aucID, currentBuyer.getUserID(), amount, currDate.Date);
+                        Console.WriteLine("--Your bid has been successfully placed");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid response please try again");
+                //just recalled the method to avoid another loop
+                bidOnItem();
+                Thread.Sleep(1000);
+            }
+        }
+        //will get specific Bid details from amount and buyerID
+        private Bid findBid(double amt)
+        {
+            foreach (Auction auc in activeAuctions)
+            {
+                foreach (Bid bid in auc.getAllBids())
+                {
+                    if (bid.getAmount() == amt)
+                    {
+                        return bid;
+                    }
+
+                }
+            }
+        }
+        private void populateAuctions()
+        {
+            foreach (Auction auc in activeAuctions)
+            {
+                if (auc.getState().Equals(States.ACTIVE))
+                {
+                    activeAuctions.Add(auc);
+                }
+            }
+        }
         private User findUsername(string username)
         {
             foreach (User user in allUsers)
@@ -201,8 +341,8 @@ namespace eAuction_System
             }
             */
         }
-        //return a Seller with specified username if there is one, otherwise returns null
-        public Seller getSellerByName(String username)
+        //return a Seller with specified username
+        private Seller getSellerByName(String username)
         {
             foreach (User user in allUsers)
             {
@@ -220,8 +360,8 @@ namespace eAuction_System
             }
             return null;
         }
-        //return a Buyer with specified username if there is one, otherwise returns null
-        public Buyer getBuyerByName(String username)
+        //return a Buyer with specified username
+        private Buyer getBuyerByName(String username)
         {
             foreach (User user in allUsers)
             {
@@ -240,7 +380,7 @@ namespace eAuction_System
             return null;
 
         }
-        public bool emptyCheck(string input)
+        private bool emptyCheck(string input)
         {
             if (String.IsNullOrEmpty(input))
             {
@@ -251,7 +391,7 @@ namespace eAuction_System
                 return false;
             }
         }
-        public bool isAlphabet(string inputString)
+        private bool isAlphabet(string inputString)
         {
             Regex r = new Regex("^[a-zA-Z ]+$");
             if (r.IsMatch(inputString))
@@ -259,6 +399,10 @@ namespace eAuction_System
             else
                 return false;
         }
+        //TODO: implementing this just because there will repetition in the menu control for the different menus
+        private void menuControl()
+        {
 
+        }
     }
 }
